a1eb21f6556294550419c253bf81fd5ea9cd2187
zy11842 <d.wang.13@warwick.ac.uk>
2017-04-28 20:37:16 +0100

    javascript added

---
+++ onclick.js
@@ -0,0 +1,56 @@
+<script>
+var a=0 
+
+function function_add_strawberrycake() {
+    document.getElementById("strawberrycake").innerHTML = "Strawberry Cake";
+    document.getElementById("strawberrycake1").innerHTML = a=a+1;
+}
+function function_minus_strawberrycake() {
+    document.getElementById("strawberrycake1").innerHTML = a=a-1;
+}
+
+
+var b=0
+
+function function_add_greenbeancake() {
+    document.getElementById("greenbeancake").innerHTML = "Green Bean Cake";
+    document.getElementById("greenbeancake1").innerHTML = b=b+1;
+}
+function function_minus_greenbeancake() {
+    document.getElementById("greenbeancake1").innerHTML = b=b-1;
+}
+
+
+var c=0
+
+function function_add_blueberrymuffin() {
+    document.getElementById("blueberrymuffin").innerHTML = "Blueberry Muffin";
+    document.getElementById("blueberrymuffin1").innerHTML = c=c+1;
+}
+function function_minus_blueberrymuffin() {
+    document.getElementById("blueberrymuffin1").innerHTML = c=c-1;
+}
+
+
+
+var d=0
+
+function function_add_walnutcookie() {
+    document.getElementById("walnutcookie").innerHTML = "Walnut Cookie";
+    document.getElementById("walnutcookie1").innerHTML = d=d+1;
+}
+function function_minus_walnutcookie() {
+    document.getElementById("walnutcookie1").innerHTML = d=d-1;
+}
+
+
+var e=0
+
+function function_add_cranberrycookie() {
+    document.getElementById("cranberrycookie").innerHTML = "Cranberry Cookie";
+    document.getElementById("cranberrycookie1").innerHTML = e=e+1;
+}
+function function_minus_cranberrycookie() {
+    document.getElementById("cranberrycookie1").innerHTML = e=e-1;
+}
+</script>
